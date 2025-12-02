da4d996da7f9cc09229670c803f674b7cc4c736b
Evelynn <evelynnbr24@gmail.com>
2025-12-02 19:42:03 -0300

    feat: função para deletar time concluído

---
+++ src/controllers/team-controller/team-delete-controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
+
+class TeamDeleteController {
+  async delete(req: Request, res: Response){
+    const paramsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = paramsSchema.parse(req.params)
+
+    const team_id = await prisma.teams.findUnique({
+      where: { id }
+    })
+
+    if(!team_id){
+      throw new AppError("team not found", 404)
+    }
+
+    await prisma.teams.delete({
+      where: { id: id }
+    })
+
+    return res.status(200).json({ message: "OK"})
+  }
+}
+
+export { TeamDeleteController }
