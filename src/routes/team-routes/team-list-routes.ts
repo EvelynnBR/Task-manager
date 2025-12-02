f8c62856c2ada4c72f4274e21dfc36b04845296f
Evelynn <evelynnbr24@gmail.com>
2025-12-02 19:21:23 -0300

    feat:  passando a função para verificar a autorização

--- src/routes/team-routes/team-list-routes.ts
+++ src/routes/team-routes/team-list-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { TeamListController } from "@/controllers/team-controller/team-list-controller"
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const teamListRoutes = Router()
 const teamListController = new TeamListController()
 
-teamListRoutes.get("/", teamListController.index)
+teamListRoutes.get("/", ensureAuthenticated, verifyUserAuthorization(["admin"]), teamListController.index)
 
 export { teamListRoutes }
