f4fd3588b90af8f5f9e5071f4b72534d51c4d6a5
Evelynn <evelynnbr24@gmail.com>
2025-12-01 22:25:54 -0300

    feat: verificando se o user tem autorização

---
+++ src/middlewares/verifyUserAuthorization.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+
+function verifyUserAuthorization(role: string[]){
+  return (req: Request, res: Response, next: NewableFunction) => {
+    if(!req.user){
+      throw new AppError("Unauthorized", 401)
+    }
+
+    if (role.includes(req.user.role)){
+      throw new AppError("Unauthorized", 401)
+    }
+
+    return next()
+  }
+
+}
