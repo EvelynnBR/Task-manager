96f98667673e8587566f363255719a2be996a75d
Evelynn <evelynnbr24@gmail.com>
2025-12-02 17:43:08 -0300

    fix: renomeado o import de TeamCreateController

--- src/routes/team-routes/team-routes.ts
+++ src/routes/team-routes/team-routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express"
-import { TeamController } from "@/controllers/team-controller/team-controller"
+import { TeamCreateController } from "@/controllers/team-controller/team-create-controller"
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const teamRoutes = Router()
-const teamController = new TeamController()
+const teamCreateController = new TeamCreateController()
 
-teamRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["admin"]), teamController.create)
+teamRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["admin"]), teamCreateController.create)
 
 
 export { teamRoutes }
