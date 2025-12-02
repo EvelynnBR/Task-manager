bb20cb50c0516449066ef9c8b925eb5ca3b5390e
Evelynn <evelynnbr24@gmail.com>
2025-12-02 17:56:44 -0300

    fix: corrigindo nome da const teamCreateRoutes

--- src/routes/team-routes/team-create-routes.ts
+++ src/routes/team-routes/team-create-routes.ts
@@ -3,10 +3,10 @@ import { TeamCreateController } from "@/controllers/team-controller/team-create-
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
-const teamRoutes = Router()
+const teamCreateRoutes = Router()
 const teamCreateController = new TeamCreateController()
 
-teamRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["admin"]), teamCreateController.create)
+teamCreateRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["admin"]), teamCreateController.create)
 
 
-export { teamRoutes }
+export { teamCreateRoutes }
