cf316f49fd74df52efddfd8204bb111aa5a1c253
Evelynn <evelynnbr24@gmail.com>
2025-12-01 21:50:27 -0300

    feat: rota para session

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { userRoutes } from "@/routes/user-routes"
+import { sessionRoutes } from "./session-routes"
 
 const routes = Router()
 
 routes.use("/users", userRoutes)
+routes.use("/session", sessionRoutes)
 
 export { routes }
 
