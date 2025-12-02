656a06e70466ae386e0ab1f4087353212a9eb9eb
Evelynn <evelynnbr24@gmail.com>
2025-12-02 17:57:49 -0300

    fix: corrigindo nomes da import de teamCreateRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from "express"
 import { userRoutes } from "@/routes/user-routes"
 import { sessionRoutes } from "./session-routes"
 
-import { teamRoutes } from "./team-routes/team-create-routes"
+import { teamCreateRoutes } from "./team-routes/team-create-routes"
 
 const routes = Router()
 
 routes.use("/users", userRoutes)
 routes.use("/session", sessionRoutes)
-routes.use("/create-team", teamRoutes)
+routes.use("/create-team", teamCreateRoutes)
 
 export { routes }
 
