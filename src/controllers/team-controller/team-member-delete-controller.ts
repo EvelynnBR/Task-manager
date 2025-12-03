479c4a718bb6a5967ecfc141837afc2ae16f5e8c
Evelynn <evelynnbr24@gmail.com>
2025-12-03 16:26:04 -0300

    feat:  função para deletar membro da equipe concluído

---
+++ src/controllers/team-controller/team-member-delete-controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { z } from "zod"
+
+class TeamMemberDeleteController {
+  async delete(req: Request, res: Response) {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(req.params)
+
+    const exists = await prisma.teamMembers.findFirst({
+      where: { id },
+    })
+
+    if (!exists) {
+      throw new AppError("Member team not exist", 401)
+    }
+
+    await prisma.teamMembers.delete({
+      where: { id },
+    })
+
+    res.status(201).json({message: "Removed"})
+  }
+}
+export { TeamMemberDeleteController }
