216884ac157bcca776151358e4ebd50229cb28f0
Evelynn <evelynnbr24@gmail.com>
2025-12-04 18:27:32 -0300

    feat: atualização de status concluído

---
+++ src/controllers/task/task-status-controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+
+class TaskStatusController {
+  async status(req: Request, res: Response) {
+    const userFromRoleToken = req.user.role
+    const userFromIdToken = req.user.id
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id: userId } = paramsSchema.parse(req.params)
+
+    if (userFromRoleToken !== "admin" && userFromIdToken !== userId) {
+      throw new AppError("You can only list tasks for yourself", 403)
+    }
+
+    const user = await prisma.user.findFirst({
+      where: { id: userId },
+    })
+
+    if (!user) {
+      throw new AppError("user not found", 404)
+    }
+
+    const bodySchema = z.object({
+      id: z.string().uuid(),
+      status: z.enum(["pending", "in_progress", "completed"]),
+    })
+
+    const { id: taskId, status } = bodySchema.parse(req.body)
+
+    const task = await prisma.tasks.findFirst({
+      where: { id: taskId },
+    })
+
+    if (!task) {
+      throw new AppError("task not found", 404)
+    }
+
+    await prisma.tasks.update({
+      where: { id: taskId },
+      data: { status },
+    })
+
+    return res.status(200).json({ message: "OK" })
+  }
+}
+
+export { TaskStatusController }
